33721b510d7059080dff5850a0d3eff8302c4df9
Robothy <robothyluo@gmail.com>
2020-10-30 18:12:09 +0800

    add String + experiment

---
+++ string-with-plus-operator/StringWithPlusOperator.java
@@ -0,0 +1,15 @@
+class StringWithPlusOperator{
+	
+	public static void main(String args){
+		
+		String str = "Hello";
+		
+		str += " Robothy";
+		
+		for(int i=0; i<10; i++){
+			str += String.valueOf(i);
+		}
+		
+	}
+	
+}
